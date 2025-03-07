7a7268dcb97c1202dc3ffd1b9ed1630171bf1ca5
Bertolucci52 <r_barbato@hotmail.com>
2025-03-07 18:34:27 +0100

    introduzione e-tivity

---
+++ Etivity1/E-Tivity_1.py
@@ -0,0 +1,4 @@
+# Programma di ricerca parametro su più file (e nei singoli file) + copia degli stessi in una dir prestabilita
+print("Nome: Roberto - Cognome: Barbato - Matricola: IN32000164")
+print("--------------------------------------------------------")
+print("Python - E-Tivity 1")
